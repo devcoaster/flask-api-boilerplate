6fd3d80955c399deaeb7eaed7f8f77ee3424e86b
Patrick Mutuku <20370328+pattoM@users.noreply.github.com>
2022-04-09 10:09:30 -0700

    Simple demo deploy to heroku

--- app/utilities.py
+++ app/utilities.py
@@ -1,8 +1,10 @@
 """
 Common utilities.
 """
+import os
 from flask import Flask
 from dataclasses import dataclass
+from dotenv import dotenv_values
 
 
 @dataclass
@@ -15,6 +17,19 @@ class AppConfig:
         Validations for config values e.g database uri is correct etc.
         """
         pass
+
+
+def load_raw_config() -> dict:
+    """
+    If env file is present, parse values from env file
+    otherwise attempt to extract them from environment.
+    """
+    env_vals = dict(dotenv_values(".env"))
+    if not env_vals:
+        env_vals["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+        env_vals["DEBUG"] = os.getenv("DEBUG")
+
+    return env_vals
 
 
 def register_routes(app: Flask, routes: list) -> None:
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 """
 Run app.
 """
-from dotenv import dotenv_values
 from app.app import create_app
-from app.utilities import AppConfig
+from app.utilities import AppConfig, load_raw_config
 
 
 # Load raw config from .env file and load onto config dataclass for extra validation.
-raw_config = dict(dotenv_values(".env"))
+raw_config = load_raw_config()
 app_config = AppConfig(**raw_config)
 
 app = create_app(app_config)
+
+app.run()
